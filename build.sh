dffbf5cc393d4532b59b4d2b3d1900e04a5c8d30
springliao <springliao@tencent.com>
2022-05-11 11:13:13 +0800

    fix: 添加release脚本

--- build.sh
+++ build.sh
@@ -1,3 +1,34 @@
 #!/bin/bash
 
+set -e
 
+if [ $# -gt 0 ]; then
+    version="$1"
+else
+    current=$(date "+%Y-%m-%d %H:%M:%S")
+    timeStamp=$(date -d "$current" +%s)
+    currentTimeStamp=$(((timeStamp * 1000 + 10#$(date "+%N") / 1000000) / 1000))
+    version="$currentTimeStamp"
+fi
+workdir=$(dirname $(realpath $0))
+
+folder_name="polaris-controller-release_${version}"
+pkg_name="${folder_name}.zip"
+
+cd $workdir
+
+# 清理环境
+rm -rf ${folder_name}
+rm -f "${pkg_name}"
+
+# 打包
+mkdir -p ${folder_name}
+cp -r deploy ${folder_name}
+zip -r "${pkg_name}" ${folder_name}
+#md5sum ${pkg_name} > "${pkg_name}.md5sum"
+
+if [[ $(uname -a | grep "Darwin" | wc -l) -eq 1 ]]; then
+    md5 ${pkg_name} >"${pkg_name}.md5sum"
+else
+    md5sum ${pkg_name} >"${pkg_name}.md5sum"
+fi
