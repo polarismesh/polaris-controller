6c44f4b91c169a5c9fa4fb7c3301b957618f8ad2
liaochuntao <liaochuntao@live.com>
2024-01-30 16:28:07 +0800

    fix:修复java-agent注入时plugin.enable格式不对 (#165)

--- pkg/inject/pkg/kube/inject/apply/javaagent/patch.go
+++ pkg/inject/pkg/kube/inject/apply/javaagent/patch.go
@@ -99,7 +99,7 @@ func (pb *PodPatchBuilder) handleJavaAgentInit(opt *inject.PatchOptions, pod *co
 	}
 	add.Env = append(add.Env, corev1.EnvVar{
 		Name:  "JAVA_AGENT_PLUGIN_TYPE",
-		Value: pluginType,
+		Value: "plugins.enable=" + pluginType,
 	})
 	kubeClient := opt.KubeClient
 	pluginCm, err := kubeClient.CoreV1().ConfigMaps(util.RootNamespace).Get(context.Background(),
